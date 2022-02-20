69f9278d0430dc5b74a02597fe3bbd229d991812
jim <daftjim1@gmail.com>
2022-02-20 09:49:58 +0100

    Change init.sql

--- DB/init.sql
+++ DB/init.sql
@@ -1,6 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Feb 16, 2022 at 10:26 PM
+-- Server version: 8.0.28
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `smashtournament`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `combat`
+--
+
+CREATE TABLE `combat` (
+  `id` int NOT NULL,
+  `idUser1` int NOT NULL,
+  `IdChar1` int NOT NULL,
+  `idUser2` int NOT NULL,
+  `idChar2` int NOT NULL,
+  `winner` int NOT NULL,
+  `niveau` varchar(5) NOT NULL,
+  `idTournoi` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personnage`
+--
+
+CREATE TABLE `personnage` (
+  `id` int NOT NULL,
+  `nom` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tournoi`
+--
+
+CREATE TABLE `tournoi` (
+  `id` int NOT NULL,
+  `date` date NOT NULL,
+  `etat` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -9,17 +68,10 @@
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `pseudo` varchar(30) NOT NULL,
   `photo` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-COMMIT;
 
 --
 -- Dumping data for table `user`
@@ -34,9 +86,52 @@ INSERT INTO `user` (`id`, `pseudo`, `photo`) VALUES
 --
 
 --
+-- Indexes for table `combat`
+--
+ALTER TABLE `combat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personnage`
+--
+ALTER TABLE `personnage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tournoi`
+--
+ALTER TABLE `tournoi`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `combat`
+--
+ALTER TABLE `combat`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tournoi`
+--
+ALTER TABLE `tournoi`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
